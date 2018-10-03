199d80d0049383c00639c26e3bde09c73e9f450a
smav <simon@mav.ec>
2018-10-03 10:42:40 +0200

    some changes to the UI, central design file

---
+++ lib/Design.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+
+  const Colors();
+
+  static const Color appBarTitle = const Color(0xFFFFFFFF);
+  static const Color appBarIconColor = const Color(0xFFFFFFFF);
+  static const Color appBarDetailBackground = const Color(0x00FFFFFF);
+  //static const Color appBarGradientStart = const Color(0xFF3383FC);
+  //static const Color appBarGradientEnd = const Color(0xFF00C6FF);
+
+  //static const Color planetCard = const Color(0xFF434273);
+  static const Color dutyCard = const Color(0xFFFFFFFFF);
+  //static const Color planetListBackground = const Color(0xFF3E3963);
+  static const Color mainBackground = const Color(0xFF736AB7);
+  static const Color dutyTitle = const Color(0xFFB41218);
+  static const Color dutyLocation = const Color(0x66B41218);
+  static const Color planetDistance = const Color(0x66B41218);
+
+  //light theme
+  static const Color whiteSmoke = const Color(0xFFF4F4F4);
+  static const Color platinum = const Color(0xFFE6E6E9);
+  static const Color spanishGrey = const Color(0xFF9999A1);
+  static const Color graniteGrey = const Color(0xFF66666E);
+  static const Color black = const Color(0xFF000000);
+
+  //forest
+  static const Color deer = const Color(0xFFB38A58);
+  static const Color spanishBistro = const Color(0xFF6F732F);
+  static const Color rifleGreen = const Color(0xFF3C5233);
+  static const Color greenLeatherJacket = const Color(0xFF264027);
+  static const Color darkJungleGreen = const Color(0xFF0D1F22);
+
+  //pastel
+  static const Color vividTangerine = const Color(0xFFF79D84);
+  static const Color rajah = const Color(0xFFFAC05E);
+  static const Color maximumBlue = const Color(0xFF3FA7D6);
+  static const Color oceanGreen = const Color(0xFF59CD90);
+  static const Color sunsetOrange = const Color(0xFFEE6352);
+
+  //blue
+  static const Color paleCornflowerBLue = const Color(0xFFA7CCED);
+  static const Color blueJeans = const Color(0xFF63ADF2);
+  static const Color airSuperiorityBlue = const Color(0xFF82A0BC);
+  static const Color blackCoral = const Color(0xFF545E75);
+  static const Color independenceBlue = const Color(0xFF304D6D);
+
+
+
+
+
+
+
+
+}
+
+class Dimens {
+  const Dimens();
+
+  static const planetWidth = 100.0;
+  static const planetHeight = 100.0;
+}
+
+class TextStyles {
+
+  const TextStyles();
+
+  static const TextStyle appBarTitle = const TextStyle(
+      color: Colors.appBarTitle,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w600,
+      fontSize: 36.0
+  );
+
+  static const TextStyle dutyTitle = const TextStyle(
+      color: Colors.dutyTitle,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w600,
+      fontSize: 24.0
+  );
+
+  static const TextStyle dutyLocation = const TextStyle(
+      color: Colors.dutyLocation,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w300,
+      fontSize: 14.0
+  );
+
+  static const TextStyle planetDistance = const TextStyle(
+      color: Colors.planetDistance,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w300,
+      fontSize: 12.0
+  );
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-// Color backRed = const Color.fromARGB(220, 220, 220, 220);
+import 'package:test_flttr/Design.dart' as Design;
 
 void main() => runApp(MyApp());
 
@@ -9,43 +8,219 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-
-        appBar: AppBar(title: Text('Dienstplan FlltrTest')),
+        backgroundColor: Design.Colors.whiteSmoke,
+        appBar: AppBar(
+          title: Text('Dienstplan Flltr Test 0'),
+          backgroundColor: Colors.grey,
+        ),
         body: Column(
           children: [
-
-            Container(
-              width: 1000.0,
-              margin: EdgeInsets.all(2.0),
-              child: Card(
-                child: Column(children: <Widget>[
-                  Text('Mittwoch | 03.10.2018 | 18:00 - 06:00 Uhr | Kalsdorf | RTW | Kerschner Benjamin, Ing.'),
-                ]),
-              ),
+            DutyPanel(
+              day: 'Mittwoch',
+              date: '03.10.2018',
+              time: '18:00 - 06:00 Uhr',
+              place: 'Kalsdorf',
+              unitType: 'RTW',
+              crew: 'Kerschner Benjamin, Ing.',
             ),
-
-            Container(
-              width: 1000.0,
-              margin: EdgeInsets.all(2.0),
-              child: Card(
-                child: Column(children: <Widget>[
-                  Text('Mittwoch | 03.10.2018 | 18:00 - 06:00 Uhr | Kalsdorf | RTW | Kerschner Benjamin, Ing.'),
-                ]),
-              ),
-            ),
-
-            Container(
-              width: 1000.0,
-              margin: EdgeInsets.all(2.0),
-              child: Card(
-                child: Column(
-                  children: <Widget>[Text('Sonntag | 14.10.2018 | 18:00 - 06:00 Uhr | Kalsdorf | RTW')],
-                ),
-              ),
-            ),
+            DutyPanel(
+                day: 'Sonntag',
+                date: '28.10.2018',
+                time: '18:00 - 06:00 Uhr',
+                place: 'Kalsdorf',
+                unitType: 'RTW',
+                crew: ''),
+            DutyNew(
+              day: 'Sonntag',
+              date: '14.10.2018',
+              time: '18:00 - 06:00 Uhr',
+              place: 'Kalsdorf',
+              unitType: 'RTW',
+              crew: '',
+            )
           ],
         ),
       ),
+    );
+  }
+}
+
+class DutyNew extends StatelessWidget {
+  DutyNew(
+      {this.day, this.date, this.time, this.place, this.unitType, this.crew});
+
+  final String day;
+  final String date;
+  final String time;
+  final String place;
+  final String unitType;
+  final String crew;
+
+  @override
+  Widget build(BuildContext context) {
+    final dutyCardNew = new Container(
+      margin: const EdgeInsets.only(left: 0.5, right: 0.5),
+      decoration: new BoxDecoration(
+        color: Design.Colors.blueJeans,
+        shape: BoxShape.rectangle,
+        borderRadius: new BorderRadius.circular(8.0),
+        boxShadow: <BoxShadow>[
+          new BoxShadow(
+              color: Colors.grey,
+              blurRadius: 500.0,
+              offset: new Offset(0.0, 10.0))
+        ],
+      ),
+      child: new Container(
+        margin: const EdgeInsets.only(top: 16.0, left: 10.0),
+        constraints: new BoxConstraints.expand(),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(date, style: Design.TextStyles.dutyTitle),
+            new Text(day + ' ' + time, style: Design.TextStyles.dutyLocation),
+/*
+            new Container(
+                color: const Color(0xFF00C6FF),
+                width: 24.0,
+                height: 1.0,
+                margin: const EdgeInsets.symmetric(vertical: 8.0)),
+  */
+            new Row(
+              children: <Widget>[
+                new Icon(Icons.location_on,
+                    size: 14.0, color: Design.Colors.planetDistance),
+                new Text(place, style: Design.TextStyles.planetDistance),
+                new Container(width: 24.0),
+                new Icon(Icons.drive_eta,
+                    size: 14.0, color: Design.Colors.planetDistance),
+                new Text(unitType, style: Design.TextStyles.planetDistance),
+              ],
+            ),
+            new Row(
+              children: <Widget>[
+                new Icon(Icons.people,
+                    size: 14.0, color: Design.Colors.planetDistance),
+                new Text(crew, style: Design.TextStyles.planetDistance),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+
+    return new Container(
+      height: 100.0,
+      margin: const EdgeInsets.only(top: 16.0, bottom: 4.0),
+      child: new FlatButton(
+        onPressed: () {},
+        child: new Stack(
+          children: <Widget>[
+            dutyCardNew,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DutyPanel extends StatelessWidget {
+  DutyPanel(
+      {this.day, this.date, this.time, this.place, this.unitType, this.crew});
+
+  final String day;
+  final String date;
+  final String time;
+  final String place;
+  final String unitType;
+  final String crew;
+
+  @override
+  Widget build(BuildContext context) {
+    final dutyPanel = new Container(
+      child: new ExpansionPanelList(
+        key: key,
+        children: <ExpansionPanel>[
+          new ExpansionPanel(
+            headerBuilder: (BuildContext context, bool isExpanded) =>
+                new Text(date + ' - ' + time),
+            body: new Container(
+              child: new Column(
+                children: <Widget>[
+                  new Row(
+                    children: <Widget>[
+                      new Icon(Icons.location_on,
+                          size: 14.0, color: Design.Colors.planetDistance),
+                      new Text(place, style: Design.TextStyles.planetDistance),
+                      new Container(width: 24.0),
+                      new Icon(Icons.drive_eta,
+                          size: 14.0, color: Design.Colors.planetDistance),
+                      new Text(unitType,
+                          style: Design.TextStyles.planetDistance),
+                    ],
+                  ),
+                  new Row(
+                    children: <Widget>[
+                      new Icon(Icons.people,
+                          size: 14.0, color: Design.Colors.planetDistance),
+                      new Text(crew, style: Design.TextStyles.planetDistance),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            isExpanded: true,
+          ),
+        ],
+        animationDuration: kThemeAnimationDuration,
+      ),
+    );
+
+    return dutyPanel;
+  }
+}
+
+class DutyContainer extends StatefulWidget {
+  @override
+  DutyContainerState createState() => new DutyContainerState();
+}
+
+class DutyItem {
+  DutyItem({this.isExpanded: false, this.header, this.body});
+
+  bool isExpanded;
+  final String header;
+  final String body;
+}
+
+class DutyContainerState extends State<DutyContainer> {
+  List<DutyItem> _items = <DutyItem>[
+    new DutyItem(header: 'Duty header Info', body: 'Duty body Info')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+      children: [
+        new ExpansionPanelList(
+          expansionCallback: (int index, bool isExpanded) {
+            setState(() {
+              _items[index].isExpanded = !_items[index].isExpanded;
+            });
+          },
+          children: _items.map((DutyItem item) {
+            return new ExpansionPanel(
+              headerBuilder: (BuildContext context, bool isExpanded) {
+                return new Text(item.header);
+              },
+              isExpanded: item.isExpanded,
+              body: new Container(
+                child: new Text('whatever'),
+              ),
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 }
